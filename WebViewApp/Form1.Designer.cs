62c87e6869276d3ec4b8182edf712e440915317a
rhofer <rhofer@pointout.de>
2018-06-19 20:17:40 +0200

    Project created

--- WebViewApp/Form1.Designer.cs
+++ WebViewApp/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.webView1 = new Microsoft.Toolkit.Win32.UI.Controls.WinForms.WebView();
+            ((System.ComponentModel.ISupportInitialize)(this.webView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView1
+            // 
+            this.webView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView1.Location = new System.Drawing.Point(0, 0);
+            this.webView1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webView1.Name = "webView1";
+            this.webView1.Size = new System.Drawing.Size(800, 450);
+            this.webView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Microsoft.Toolkit.Win32.UI.Controls.WinForms.WebView webView1;
     }
 }
 
--- WebViewApp/Form1.cs
+++ WebViewApp/Form1.cs
@@ -16,5 +16,10 @@ namespace WebViewApp
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webView1.Navigate("https://login2.mse-solutions.com/pointout14");
+        }
     }
 }
